d3d10a3166d48052f7f7c2c5ac892d08f85377a2
Jamie W <jamie.won.2000@gmail.com>
2019-01-26 19:33:35 -0500

    linking to database

--- src/loadInitial.js
+++ src/loadInitial.js
@@ -1,6 +1,30 @@
-// const admin = require('firebase-admin');
-// const functions = require('firebase-functions');
 
-// admin.initializeApp(functions.config().firebase);
+//       var config = {
+//         apiKey: "AIzaSyAc14wfNVZScC-9u1YPqwEfCC09v3KE49Q",
+//         authDomain: "conuhacks2019-d7b13.firebaseapp.com",
+//         databaseURL: "https://conuhacks2019-d7b13.firebaseio.com",
+//         projectId: "conuhacks2019-d7b13",
+//         storageBucket: "conuhacks2019-d7b13.appspot.com",
+//         messagingSenderId: "992278114219"
+//       };
+//       firebase.initializeApp(config);
+// // const admin = require('firebase-admin');
+// // const functions = require('firebase-functions');
 
-// var db = admin.firestore();
+// // var db = firebase.database();
+// var db = firebase.firestore();
+// // admin.initializeApp(functions.config().firebase);
+
+// <script src="https://www.gstatic.com/firebasejs/5.5.5/firebase-app.js"></script>;
+// <script src="https://www.gstatic.com/firebasejs/5.5.5/firebase-firestore.js"></script>;
+
+
+firebase.initializeApp({
+ apiKey: "AIzaSyAc14wfNVZScC-9u1YPqwEfCC09v3KE49Q",
+        authDomain: "conuhacks2019-d7b13.firebaseapp.com",
+//         databaseURL: "https://conuhacks2019-d7b13.firebaseio.com",
+        projectId: "conuhacks2019-d7b13",
+});
+
+// Initialize Cloud Firestore through Firebase
+var db = firebase.firestore();
